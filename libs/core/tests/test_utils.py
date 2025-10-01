01423ba1edd2f5d30136140ed098fe2baa60f244
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2025-10-01 16:24:14 -0700

    fix test failure in core

--- libs/core/tests/test_utils.py
+++ libs/core/tests/test_utils.py
@@ -10,7 +10,7 @@ from sim_sci_test_monorepo.core.utils import CoreUtility, hello_core
 def test_hello_core():
     """Test the hello_core function."""
     result = hello_core()
-    assert result == "Hello from sim_sci_test_monorepo.core!"
+    assert result == "Hello from sim_sci_test_monorepo.core! This is a new version!"
 
 
 def test_core_utility():
