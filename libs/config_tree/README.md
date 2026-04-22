ecac31988a220a75894a493ed4c0a9d16807fbc6
Steve Bachmeier <sbachmei@uw.edu>
2026-04-22 10:11:23 -0600

    add config-tree to see if jenkins auto-finds it

---
+++ libs/config_tree/README.md
@@ -0,0 +1,5 @@
+# sim_sci_test_monorepo - config_tree
+
+Configuration tree functionality for the sim_sci_test_monorepo.
+
+This package exists to test Jenkins auto-provisioning of new libs in the monorepo.
---
+++ libs/config_tree/src/sim_sci_test_monorepo/config_tree/__init__.py
@@ -0,0 +1,9 @@
+"""
+Configuration tree functionality for sim_sci_test_monorepo
+"""
+
+try:
+    from ._version import __version__
+except ImportError:
+    # Package is not installed
+    __version__ = "unknown"
